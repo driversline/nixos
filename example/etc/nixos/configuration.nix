68c1813fc90214ec42b7a30850826287d679c0ea
driversline <n526258360@gmail.com>
2025-02-09 11:26:38 +0300

    configuration.nix

--- example/etc/nixos/configuration.nix
+++ example/etc/nixos/configuration.nix
@@ -79,31 +79,6 @@
 
   time.timeZone = "Europe/Moscow";
 
-# - - - - - - Locale - - - - - - #
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-  services.xserver.xkb.layout = "us,ru";
-  services.xserver.xkb.options = "grp:alt_shift_toggle";
-
-   console = {
-     font = "Lat2-Terminus16";
-     keyMap = "us";
-     useXkbConfig = false;
-   };
-
 # - - - - - - Docker - - - - - - #
 
 #   virtualisation.docker.enable = true;
