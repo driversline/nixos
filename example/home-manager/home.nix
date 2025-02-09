2ec4cc91130c0060704d965af40b25ff80a57e9b
driversline <n526258360@gmail.com>
2025-02-09 12:11:48 +0300

    home.nix

---
+++ example/home-manager/home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  home.stateVersion = "24.11";
+  home.username = "user";
+  home.homeDirectory = "/home/user";
+
+  home.pointerCursor =
+    let
+      getFrom = url: hash: name: {
+          gtk.enable = true;
+          x11.enable = true;
+          name = name;
+          size = 18;
+          package =
+            pkgs.runCommand "moveUp" {} ''
+              mkdir -p $out/share/icons
+              ln -s ${pkgs.fetchzip {
+                url = url;
+                sha256 = "7yY1yDUzNg+SJynv7WFdAYApOFGFc7T3DT1C/auGORQ=";
+              }} $out/share/icons/${name}
+          '';
+        };
+    in
+      getFrom
+        "https://github.com/driversline/bspwm/blob/main/dotfiles/cursors/Nordzy-cursors-white.tar.gz?raw=true"
+        "7yY1yDUzNg+SJynv7WFdAYApOFGFc7T3DT1C/auGORQ="
+        "Nordzy-cursors-white";
+
+  home.sessionVariables = {
+   XCURSOR_THEME = "Nordzy-cursors-white";
+  };
+
+}
