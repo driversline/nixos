433d8dcf849bd770c8ffa1dd9c10834372edbc00
driversline <n526258360@gmail.com>
2025-02-09 11:31:49 +0300

    locale.nix

--- example/etc/nixos/locale.nix
+++ example/etc/nixos/locale.nix
@@ -3,18 +3,6 @@
 {
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
   services.xserver.xkb.layout = "us,ru";
   services.xserver.xkb.options = "grp:alt_shift_toggle";
 
