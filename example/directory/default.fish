eabfd2961c7ccc9f4ae79d92df3bc7ba997af31f
driversline <n526258360@gmail.com>
2025-02-09 11:32:53 +0300

    default.fish

--- example/directory/default.fish
+++ example/directory/default.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function create_dirs
-    mkdir -p ~/.config/gtk-3.0 ~/.config/alacritty ~/.config/bspwm
+    mkdir -p ~/.config/gtk-3.0 ~/.config/alacritty ~/.config/bspwm ~/.config/nvim
 end
 
 create_dirs
