13fb18a879156229ca24cafd13bcfac86bc3d203
driversline <n526258360@gmail.com>
2025-02-14 18:34:52 +0300

    flake.nix

---
+++ flake/nixos/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS configuration with various modules";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      nix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./sxhkd.nix
+          ./nvidia.nix
+          ./network.nix
+          ./neovim.nix
+          ./locale.nix
+          ./home.nix
+          ./fish.nix
+          ./configuration.nix
+          ./bspwm.nix
+          ./alacritty.nix
+        ];
+      };
+    };
+  };
+}
