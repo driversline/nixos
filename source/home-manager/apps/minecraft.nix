dadaa40382e504d2a58180504833d2b3264d8736
driversline <n526258360@gmail.com>
2025-02-22 15:47:10 +0300

    minecraft.nix

--- source/home-manager/apps/minecraft.nix
+++ source/home-manager/apps/minecraft.nix
@@ -5,6 +5,11 @@
     LD_LIBRARY_PATH = "/nix/store/kynlrr98p2c235b00c72b14apn7l8l4y-libglvnd-1.7.0/lib";
   };
 
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = true;
+   };
+
   home.packages = with pkgs; [
     minecraft
     libGL
