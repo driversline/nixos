06d75f0661f36832a52b48f75a679eb2cc733e47
driversline <161175951+driversline@users.noreply.github.com>
2025-03-09 20:00:50 +0300

    configuration.nix

--- minimal/configuration.nix
+++ minimal/configuration.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
 
-  # This value determines the NixOS release with which your system is to be compatible.
-  # You should change this only after NixOS release notes say you should.
-  system.stateVersion = "24.11"; # Did you read the comment?
+imports =
+[
+  ./hardware-configuration.nix
+];
+
+system.stateVersion = "24.11";
+
 }
