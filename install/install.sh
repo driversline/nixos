6653cd46bda1c02a3773b62182813fa4e42bc48b
driversline <161175951+driversline@users.noreply.github.com>
2025-03-28 20:21:10 +0300

    Delete install directory

--- install/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-mkfs.fat -F 32 /dev/sda1 -n NIXBOOT
-mkfs.ext4 /dev/sda2 -L NIXROOT
-mount /dev/disk/by-label/NIXROOT /mnt
-mkdir -p /mnt/boot
-mount /dev/disk/by-label/NIXBOOT /mnt/boot
-nixos-generate-config --root /mnt
-nixos-install
